a8e2cf39094b63676dd5f85edf2ab92a580c650b
Terhi <salonenterhi19@gmail.com>
2020-11-27 09:23:12 +0200

    Refactor the project structure: + Add components/ into src/. + Move App.js App.css and App.test.js into components/. + Remove logo.svg and all references to it.

---
+++ medo-dodo/src/components/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Terhi was here.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
