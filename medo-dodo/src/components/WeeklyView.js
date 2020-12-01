44cdc0b2d887dcdb283bfe4285bff4486b0dad5e
haelina <56834622+haelina@users.noreply.github.com>
2020-12-01 16:58:34 +0200

    Add new component WeeklyView.
    Includes components Header, TasksWeekly and Footer.

---
+++ medo-dodo/src/components/WeeklyView.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./WeeklyView.css";
+import Header from "./Header";
+import TasksWeekly from "./TasksWeekly";
+import Footer from "./Footer";
+
+export default class WeeklyView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.showingWeek !== this.props.showingWeek) {
+      console.log("showing different week now");
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Header
+          key={1}
+          weekNumber={this.props.currentWeek}
+          page={this.props.page}
+          showingWeek={this.props.showingWeek}
+          date={this.props.currentDate}
+          onClickNext={this.props.onClickNext}
+          onClickLast={this.props.onClickLast}
+        />
+        <TasksWeekly showingWeek={this.props.showingWeek} />
+        <Footer key={2} onClickAdd={this.props.onClickAdd} />
+      </div>
+    );
+  }
+}
