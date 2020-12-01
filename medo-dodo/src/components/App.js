fd5a668db5b4f3fc62ff2f7731a0386208d60ea8
haelina <56834622+haelina@users.noreply.github.com>
2020-12-01 16:50:20 +0200

    Change App to show different views.

--- medo-dodo/src/components/App.js
+++ medo-dodo/src/components/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React from "react";
+/*
 import TextInputField from "./TextInputField";
 import Header from "./Header.js";
 import DueTime from "./DueTimeInput";
@@ -7,6 +8,8 @@ import PriorityTagList from "./PriorityTagList";
 import DropDown from "./DropDown";
 import Footer from "./Footer";
 import TasksWeekly from "./TasksWeekly";
+*/
+import WeeklyView from "./WeeklyView";
 
 class App extends React.Component {
   constructor(props) {
@@ -19,8 +22,6 @@ class App extends React.Component {
       currentPage: "weekly",
       priorities: ["high", "medium", "low"],
     };
-    this.handleNextWeek = this.handleNextWeek.bind(this);
-    this.handleLastWeek = this.handleLastWeek.bind(this);
   }
 
   componentDidMount() {
@@ -30,7 +31,18 @@ class App extends React.Component {
   onTextFieldSubmit(term) {
     console.log(term + ".");
   }
-  handleNextWeek() {
+
+  changeViewToAdd = () => {
+    this.setState({ currentPage: this.state.pages[1] });
+    console.log("Currently we are on page " + this.state.currentPage);
+  };
+
+  changeViewToWeekly = () => {
+    this.setState({ currentPage: this.state.pages[0] });
+    console.log("Currently we are on page " + this.state.currentPage);
+  };
+
+  handleNextWeek = () => {
     if (this.state.showingWeek === 53) {
       this.setState({ showingWeek: 1 });
       console.log(this.state.showingWeek);
@@ -38,8 +50,8 @@ class App extends React.Component {
       this.setState({ showingWeek: this.state.showingWeek + 1 });
       console.log(this.state.showingWeek);
     }
-  }
-  handleLastWeek() {
+  };
+  handleLastWeek = () => {
     if (this.state.showingWeek === 1) {
       this.setState({ showingWeek: 53 });
       console.log(this.state.showingWeek);
@@ -47,11 +59,54 @@ class App extends React.Component {
       this.setState({ showingWeek: this.state.showingWeek - 1 });
       console.log(this.state.showingWeek);
     }
+  };
+
+  checkView() {
+    if (this.state.currentPage === "weekly") {
+      return (
+        <WeeklyView
+          currentWeek={this.state.currentWeek}
+          page={this.state.pages[0]}
+          showingWeek={this.state.showingWeek}
+          currentDate={this.state.currentDate}
+          onClickNext={this.handleNextWeek}
+          onClickLast={this.handleLastWeek}
+          onClickAdd={this.changeViewToAdd}
+        />
+      );
+    } else {
+      return (
+        <div>
+          <h1>Something went terribly wrong xD</h1>
+          <button onClick={this.changeViewToWeekly}>Go back to weekly</button>
+        </div>
+      );
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.currentPage !== this.state.currentPage) {
+      console.log("need to change view now");
+      this.checkView();
+    }
   }
 
   render() {
+    let view = this.checkView();
     return (
       <div className="App ui container">
+        {view}
+        {/*}
+        <WeeklyView
+          currentWeek={this.state.currentWeek}
+          page={this.state.pages[0]}
+          showingWeek={this.state.showingWeek}
+          currentDate={this.state.currentDate}
+          onClickNext={this.handleNextWeek}
+          onClickLast={this.handleLastWeek}
+        />
+    */}
+        {/*
         <Header
           key={1}
           weekNumber={this.state.currentWeek}
@@ -85,6 +140,7 @@ class App extends React.Component {
           page={this.state.pages[0]}
           onClickLast={this.handleLastWeek}
         />
+        */}
       </div>
     );
   }
