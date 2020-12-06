a2db6e099361cef18654a2d090dba08e9ece785f
salter19 <salonenterhi19@gmail.com>
2020-12-06 20:32:53 +0200

    Add import of prioritylevels into PriorityTag, add also getButtonStyle() and getTitle() into it.

--- medo-dodo/src/components/PriorityTag.js
+++ medo-dodo/src/components/PriorityTag.js
@@ -1,11 +1,28 @@
 import React from "react";
-
+import priority_levels from './prioritylevels'
 class PriorityTag extends React.Component {
   state = { tagColor: "", title: "" };
 
   componentDidMount() {
     this.setState({ tagColor: this.props.level, title: this.props.title });
   }
+  getButtonStyle = (value) => {
+    return (
+        value === priority_levels[2] 
+        ? "ui red button" 
+        : value === priority_levels[1]
+        ? "ui yellow button"
+        : "ui green button" )
+    };
+  
+    getTitle = (value) => {
+      return (
+        value === priority_levels[2] 
+        ? "HIGH" 
+        : value === priority_levels[1]
+        ? "MEDIUM"
+        : "LOW" )
+    };
 
   render() {
     return (
