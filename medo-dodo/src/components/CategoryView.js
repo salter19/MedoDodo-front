fe67397aa48de520bf6126df1749bd3f50f948aa
salter19 <salonenterhi19@gmail.com>
2020-12-07 15:28:40 +0200

    Add skeleton of CategoryView into components/.

---
+++ medo-dodo/src/components/CategoryView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class CategoryView extends React.Component {
+    render() {
+        return <div className="category-view">CAT view</div>
+    }
+}
+
+export default CategoryView
