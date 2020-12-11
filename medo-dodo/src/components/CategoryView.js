12109be92071c609fdc8ef1b63030f21c4cafac5
salter19 <salonenterhi19@gmail.com>
2020-12-11 22:28:25 +0200

    Fix modify button in CategoryView.

--- medo-dodo/src/components/CategoryView.js
+++ medo-dodo/src/components/CategoryView.js
@@ -12,8 +12,9 @@ class CategoryView extends React.Component {
                     page={this.props.page}
                     onClickRight={this.props.onClickAdd} 
                     onClickLeft={this.props.onClickCats}
+                    onClickTask={this.props.onClickTask}
                     catTitle={this.props.title}
-                    view={<TasksByCatTitle catTitle={this.props.title} />}
+                    view={<TasksByCatTitle catTitle={this.props.title} onClickTask={this.props.onClickTask}/>}
                 />
             </div>
         )
