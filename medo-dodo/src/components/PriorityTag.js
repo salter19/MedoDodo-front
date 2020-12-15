2d439e8f82194d1d3df421d6d8691afb9f1be435
salter19 <salonenterhi19@gmail.com>
2020-12-15 21:38:09 +0200

    Refactor PriorityTag: change buttons to labels.

--- medo-dodo/src/components/PriorityTag.js
+++ medo-dodo/src/components/PriorityTag.js
@@ -2,26 +2,22 @@ import React from "react";
 import priority_levels from './prioritylevels'
 
 class PriorityTag extends React.Component {
-  state = { tagColor: "", title: "", chosen: true };
+  state = { tagColor: "", title: ""};
 
   componentDidMount() {
     this.setState({ 
-      tagColor: this.getButtonStyle(this.props.priorityChosen), 
-      title: this.getTitle(this.props.priorityChosen),
-      chosen: this.props.chosen
+      tagColor: this.getTagColor(this.props.priorityChosen), 
+      title: this.getTitle(this.props.priorityChosen)
     });
   }
-  getButtonStyle = (value) => {
-    const last = priority_levels.length - 1;
+  getTagColor = (value) => {
+    console.log(value)
     return (
-        value === priority_levels[last] && this.state.chosen 
-        ? "ui red button" 
-        : value === priority_levels[last - 1] && this.state.chosen
-        ? "ui yellow button"
-        : value === priority_levels[last - 2] && this.state.chosen
-        ? "ui green button"
-        : "ui grey button"
-        
+      value === priority_levels[2]
+      ? "ui red tag label" 
+      : value === priority_levels[1]
+      ? "ui yellow tag label"
+      : "ui green tag label"
       )
     };
   
@@ -34,15 +30,11 @@ class PriorityTag extends React.Component {
         : "LOW" )
     };
 
-    onInputChange = () => {
-      this.props.onTagClick(this.props.priorityChosen)
-    }
-
   render() {
     return (
       <div key={this.state.title} className="priority-tag">
         <div className={this.state.title}>
-          <div className={this.state.tagColor} onClick={this.onInputChange}>
+          <div className={this.state.tagColor} >
             {this.state.title}
           </div>
         </div>
