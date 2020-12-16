daa3f08607e391ca8d6f56e5ca047594bac9cf0a
haelina <56834622+haelina@users.noreply.github.com>
2020-12-16 13:47:51 +0200

    Refactor code by adding a variable and importing it.

--- medo-dodo/src/components/validCategoryTitles.js
+++ medo-dodo/src/components/validCategoryTitles.js
@@ -1,14 +1,15 @@
-import TasksGetter from './TasksGetter'
-const validCatTitles = async() => {
-    const catObj = await TasksGetter.everyCat()
-    const titlesArr = catObj.map(obj => obj.title)
-    return titlesArr
-}
+import TasksGetter from "./TasksGetter";
+const validCatTitles = async () => {
+  const catObj = await TasksGetter.everyCat();
+  const titlesArr = catObj.map((obj) => obj.title);
+  return titlesArr;
+};
 
-const taskTitlesByCat = async(cat) => {
-    const taskObj = await TasksGetter.byCategoryTitle(cat)
-    return taskObj
-}
+const taskTitlesByCat = async (cat) => {
+  const taskObj = await TasksGetter.byCategoryTitle(cat);
+  return taskObj;
+};
 
-export default { validCatTitles, taskTitlesByCat }
+const obj = { validCatTitles, taskTitlesByCat };
 
+export default obj;
