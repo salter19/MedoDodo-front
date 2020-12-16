ccfdf42fb0c0e1d90f49df03272a0dfc1fa85754
haelina <56834622+haelina@users.noreply.github.com>
2020-12-16 16:44:32 +0200

    Call function onDelete when delete button is pressed.

--- medo-dodo/src/components/MyButton.js
+++ medo-dodo/src/components/MyButton.js
@@ -1,56 +1,64 @@
 import "./styles/MyButton.css";
 import React from "react";
-import pagetypes from './pagetypes'
-import buttontypes from './buttontypes'
+import pagetypes from "./pagetypes";
+import buttontypes from "./buttontypes";
 
 class MyButton extends React.Component {
-  state = { buttontype: '', errormsg: 'Where are we, asks button?', buttonText:'',  }
+  state = {
+    buttontype: "",
+    errormsg: "Where are we, asks button?",
+    buttonText: "",
+  };
 
   componentDidMount() {
-    this.setState({ buttontype: this.props.buttontype })
+    this.setState({ buttontype: this.props.buttontype });
     this.setButtonText();
   }
 
   setButtonText = () => {
     this.props.buttontype === buttontypes.category
-      ? this.setState( { buttonText: this.props.category } )
-      : this.setState( { buttonText: this.props.buttontype } )
-  }
+      ? this.setState({ buttonText: this.props.category })
+      : this.setState({ buttonText: this.props.buttontype });
+  };
 
   changePage = () => {
-    this.props.page === pagetypes.addTask || this.props.page === pagetypes.modifyTask
+    this.props.page === pagetypes.addTask ||
+    this.props.page === pagetypes.modifyTask
       ? this.props.onSave()
       : this.errorHandler();
-    
+
     this.props.page === pagetypes.categories
       ? this.props.onClickCats()
-       : this.errorHandler();
- 
+      : this.errorHandler();
+
     this.props.page === pagetypes.weekly
       ? this.props.onClickWeeks()
       : this.errorHandler();
 
     this.state.buttontype === buttontypes.category
       ? this.props.onClickCat(this.props.category)
-      : this.errorHandler()
+      : this.errorHandler();
 
     this.state.buttontype === buttontypes.modify
       ? this.props.onClickTask(this.props.taskID)
+      : this.errorHandler();
+
+    this.state.buttontype === buttontypes.delete
+      ? this.props.onDelete(this.props.taskID)
       : this.errorHandler();
   };
 
   errorHandler = () => {
     //console.log(`${this.state.errormsg} ${this.props.page}`)
-  }
+  };
 
   render() {
     return (
       <div className="button">
-        <button className={this.state.buttontype} onClick={this.changePage} >
+        <button className={this.state.buttontype} onClick={this.changePage}>
           {this.state.buttonText}
         </button>
-    </div>
-      
+      </div>
     );
   }
 }
