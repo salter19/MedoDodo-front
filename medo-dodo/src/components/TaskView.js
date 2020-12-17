9d220fa156f091b1b5df27b1bbc478316709c33e
salter19 <salonenterhi19@gmail.com>
2020-12-17 14:24:42 +0200

    Clean at TaskView: remove obsolete console.logs.

--- medo-dodo/src/components/TaskView.js
+++ medo-dodo/src/components/TaskView.js
@@ -127,12 +127,11 @@ class TaskView extends React.Component {
   }
 
   setSelectedCategory = (cat) => {
-    console.log(cat)
     this.setState( { selectedCategory: cat } )    
   }
 
   setDefaultCategoryForDropdown = async(id) => {
-    console.log('this is the id: ' + id)
+    
     const categories = await TaskGetter.everyCategory();
     let res = []
     categories.map((obj) => 
