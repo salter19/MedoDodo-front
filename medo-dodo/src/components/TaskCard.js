7a1e964a36af0dccf9a11ea9ea03e07f9d1fb878
salter19 <salonenterhi19@gmail.com>
2020-12-19 20:19:34 +0200

    Add to TaskCard: another date formater.

--- medo-dodo/src/components/TaskCard.js
+++ medo-dodo/src/components/TaskCard.js
@@ -5,24 +5,32 @@ import buttontypes from "./buttontypes";
 import pagetypes from "./pagetypes";
 import TaskGetter from "./ConnectToBackend";
 
-const getFormattedDate = (date) => {
+const getFormattedDateForCard = (date) => {
   const arr = date.split(/[^0-9]/);
   const formatted = `${arr[2]}.${arr[1]}.${arr[0]} at ${arr[3]}.${arr[4]}`;
   return formatted;
 };
 
-const TaskCard = ({ id, priorityLevel, levelTitle, onClickTask }) => {
+const getFormattedDateForSave = (date) => {
+  const arr = date.split(/[^0-9]/);
+  const formatted = `${arr[0]}-${arr[1]}-${arr[2]} ${arr[3]}:${arr[4]}:${arr[5]}`;
+  return formatted;
+}
+
+const TaskCard = ({ id, priorityLevel, levelTitle, onClickTask, currentCategory }) => {
   const [title, setTitle] = useState("");
   const [dueDate, setDueDate] = useState("");
-  const [checkbox, setCheckbox] = useState(0);
+  const [done, setDone] = useState(false);
   const [task, setTask] = useState('');
 
   useEffect(() => {
     const createTask = async () => {
       const data = await TaskGetter.byId(id);
+
+      console.log(data)
       
       if (data.length > 0) {
-        const date = getFormattedDate(data[0].due_date);
+        const date = getFormattedDateForCard(data[0].due_date);
         setTitle(data[0].title);
         setDueDate(date);
         setTask(data[0]);
@@ -32,14 +40,38 @@ const TaskCard = ({ id, priorityLevel, levelTitle, onClickTask }) => {
     createTask();
   }, [id, levelTitle]);
 
-  const setDone = () => {
-    setCheckbox(1);
-    console.log(`Task ${title} is done!`);
+  const onCheckboxClick = () => {
+    setDone(!done);
+
+    // default done === false;
+    !done ? saveTaskState() : console.log('undone')
 
     console.log(task)
     
     // this will eventually lead to changing appearance of task card
   };
+
+  const saveTaskState = async() => {
+
+    getFormattedDateForSave(task.due_date);
+    const tmp = {
+      title: task.title,
+      due_date: task.due_date,
+      description: task.description,
+      priority: task.priority,
+      category_id: task.category_id,
+      is_done: done,
+    };
+    console.log(tmp);
+
+    try {
+      const saveUp = await TaskGetter.saveTask(tmp);
+      alert(saveUp);
+      this.props.onSave();
+    } catch (error) {
+      alert("Something went wrong with saving the task.");
+    }
+  }
 
   return (
     <div className="task-card">
@@ -48,7 +80,7 @@ const TaskCard = ({ id, priorityLevel, levelTitle, onClickTask }) => {
           <div className="three column centered row">
             <div className="column">
               <div className="ui checkbox">
-                <input className="box" type="checkbox" onClick={setDone} />
+                <input className="box" type="checkbox" onClick={onCheckboxClick} />
                 <label>
                   <h3>{title}</h3>
                 </label>
