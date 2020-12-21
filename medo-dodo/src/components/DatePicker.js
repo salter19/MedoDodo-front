797ef1aae8073be60fa6ab22da88f968c52b7539
Terhi <salonenterhi19@gmail.com>
2020-12-21 16:56:17 +0200

    Fix at DatePicker: a warning, one stays still.

--- medo-dodo/src/components/DatePicker.js
+++ medo-dodo/src/components/DatePicker.js
@@ -1,84 +1,75 @@
-import './styles/DatePicker.css'
-import React, {useState, useEffect} from 'react'
-import DateTimeSetter from 'react-datetime-picker'
+import "./styles/DatePicker.css";
+import React, { useState, useEffect } from "react";
+import DateTimeSetter from "react-datetime-picker";
 
-const DatePicker = ({onSelectedChange}) => {
+const DatePicker = ({ onSelectedChange }) => {
+  const [value, setValue] = useState(new Date());
 
-    const [value, setValue] = useState(new Date());
+  useEffect(() => {
+    const time = {
+      day: value.getDate(),
+      month: value.getMonth() + 1,
+      year: value.getFullYear(),
+      hour: value.getHours(),
+      minutes: value.getMinutes(),
+      seconds: value.getSeconds(),
+    };
+    const formated = formatDate(time);
+    onSelectedChange(formated);
+  }, [value, onSelectedChange]);
 
+  const formatDate = ({ day, month, year, hour, minutes, seconds }) => {
+    // 2020-11-25 21:00:00
 
-    useEffect(() => {
-        const time = {
-          day: value.getDate(), 
-          month: value.getMonth() + 1, 
-          year: value.getFullYear(), 
-          hour: value.getHours(), 
-          minutes: value.getMinutes(), 
-          seconds: value.getSeconds()
-        }
-        const formated = formatDate(time)
-        onSelectedChange(formated)
-    }, [value])
+    day = addZeroFront(day);
+    month = addZeroFront(month);
+    hour = addZeroFront(hour);
+    minutes = addZeroFront(minutes);
+    seconds = addZeroFront(seconds);
+    const formated = `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`;
+    return formated;
+  };
 
-    const formatDate = ({day, month, year, hour, minutes, seconds}) => {
-      // 2020-11-25 21:00:00
+  const addZeroFront = (num) => {
+    return num < 10 ? "0" + num : num;
+  };
 
-      day = addZeroFront(day);
-      month = addZeroFront(month);
-      hour = addZeroFront(hour);
-      minutes = addZeroFront(minutes);
-      seconds = addZeroFront(seconds);
-      const formated = `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`
-      return formated;
-    }
-
-    const addZeroFront = (num) => {
-      return num < 10 ? "0" + num : num;  
-    }
-
-    return (
-        <div className="date-picker">
-          <div className="ui segments">
-            <div className="ui segment">  
-            
-              <div className="ui form">
-                <div className="field">
-                  <label className="label">Due date and time</label>
-                </div>
-              </div>
-
-              <div className="ui grid">
-                <div className="three wide row">
-                  <DateTimeSetter 
-                      onChange={setValue}
-                      value={value}
-                      disableClock={true}
-                      clearIcon={null}
-                      required={true}
-                  />
-                </div>                
-              </div>              
-
+  return (
+    <div className="date-picker">
+      <div className="ui segments">
+        <div className="ui segment">
+          <div className="ui form">
+            <div className="field">
+              <label className="label">Due date and time</label>
             </div>
-            <div className="ui secondary segment">
-              
-              <div className="ui form">
-                <div className="field">
-                  <label className="label">Dued </label>
-                </div>
-              </div>
+          </div>
 
-              <div className="ui grid">
-                <div className="three wide row">
-                  {value ? `${value}` : ''}
-                </div>                
-              </div> 
-              
+          <div className="ui grid">
+            <div className="three wide row">
+              <DateTimeSetter
+                onChange={setValue}
+                value={value}
+                disableClock={true}
+                clearIcon={null}
+                required={true}
+              />
             </div>
-            
           </div>
         </div>
-    );
-}
+        <div className="ui secondary segment">
+          <div className="ui form">
+            <div className="field">
+              <label className="label">Dued </label>
+            </div>
+          </div>
 
-export default DatePicker
+          <div className="ui grid">
+            <div className="three wide row">{value ? `${value}` : ""}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DatePicker;
