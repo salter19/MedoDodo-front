93ce137ce13c06769e9a7e00603f9e0400e63809
Terhi <salonenterhi19@gmail.com>
2020-12-21 16:58:18 +0200

    Fix at Servers: a warning.

--- medo-dodo/src/components/Servers.js
+++ medo-dodo/src/components/Servers.js
@@ -1,4 +1,5 @@
-const local = 'http://localhost:8080/tasks/';
-const remote = 'https://tamk-4a00ez62-3001-group06.herokuapp.com/tasks/';
+const local = "http://localhost:8080/tasks/";
+const remote = "https://tamk-4a00ez62-3001-group06.herokuapp.com/tasks/";
 
-export default { local, remote }
+const res = { local, remote };
+export default res;
