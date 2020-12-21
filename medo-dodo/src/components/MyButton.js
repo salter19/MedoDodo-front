c94473edb9ffcbbb30884770e567b4b90653fc63
haelina <56834622+haelina@users.noreply.github.com>
2020-12-21 11:52:46 +0200

    Fix deleting a task a bit more.

--- medo-dodo/src/components/MyButton.js
+++ medo-dodo/src/components/MyButton.js
@@ -8,7 +8,7 @@ class MyButton extends React.Component {
     buttontype: "",
     errormsg: "Where are we, asks button?",
     buttonText: "",
-    key: ''
+    key: "",
   };
 
   componentDidMount() {
@@ -17,17 +17,25 @@ class MyButton extends React.Component {
   }
 
   setButtonText = () => {
-    
     if (this.props.buttontype === buttontypes.category) {
-      this.setState({ buttonText: this.props.category,  key: this.props.category});
+      this.setState({
+        buttonText: this.props.category,
+        key: this.props.category,
+      });
     } else if (this.props.buttontype === buttontypes.modify) {
-      this.setState({ buttonText: ["DELETE", <br />, "TASK"], key: this.props.taskID});
+      this.setState({
+        buttonText: ["DELETE", <br />, "TASK"],
+        key: this.props.taskID,
+      });
     } else if (this.props.buttontype === buttontypes.addTask) {
-      this.setState({ buttonText: ["ADD NEW", <br />, " TASK"], key: 'add' });
+      this.setState({ buttonText: ["ADD NEW", <br />, " TASK"], key: "add" });
     } else if (this.props.buttontype === buttontypes.categories) {
-      this.setState({ buttonText: ["CATEGORIES", <br />, "VIEW"], key: 'cats' });
+      this.setState({
+        buttonText: ["CATEGORIES", <br />, "VIEW"],
+        key: "cats",
+      });
     } else if (this.props.buttontype === buttontypes.weekly) {
-      this.setState({ buttonText: ["WEEKLY", <br />, "VIEW"], key: 'weeks' });
+      this.setState({ buttonText: ["WEEKLY", <br />, "VIEW"], key: "weeks" });
     } else {
       this.setState({ buttonText: this.props.buttontype });
     }
@@ -69,10 +77,13 @@ class MyButton extends React.Component {
   };
 
   render() {
-    
     return (
       <div key={this.state.key} className="button">
-        <button key={this.state.key} className={this.state.buttontype} onClick={this.changePage}>
+        <button
+          key={this.state.key}
+          className={this.state.buttontype}
+          onClick={this.changePage}
+        >
           <div key={this.state.key}>{this.state.buttonText}</div>
         </button>
       </div>
